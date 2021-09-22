ba31a0241614c77e8c92a2aa4c50f83e58106c4f
Azimatul Hanafiyah <89617795+azimatulh@users.noreply.github.com>
2021-09-22 07:23:27 -0700

    DataPenduduk

---
+++ DataPenduduk.java
@@ -0,0 +1,43 @@
+public class DataPenduduk{
+    public String nik;
+    public String nama;
+    public String ttl;
+    public String alamat;
+    public String agama;
+    public String jenisKelamin;
+    public String status;
+    
+     public void setNIK(String nik) {
+         this.nik = nik;
+     }
+     
+     public void setNama(String nama) {
+         this.nama = nama;
+     }
+     
+     public void setTTL(String ttl) {
+         this.ttl = ttl;
+     }
+     
+     public void setAlamat(String alamat) {
+         this.alamat = alamat;
+     }
+     
+     public void setAgama(String agama) {
+         this.agama = agama;
+     }
+     
+     public void setJenisKelamin(String jenisKelamin) {
+         this.jenisKelamin = jenisKelamin;
+     }
+     
+     public void setStatus(String status) {
+         this.status = status;
+     }
+     
+     public void mempermudahPencatatan(){
+     
+ }
+ 
+ 
+ }
