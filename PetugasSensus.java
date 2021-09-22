805f78693f2b7e3ad4f1cfa8cd7ea529e457a3d7
Azimatul Hanafiyah <89617795+azimatulh@users.noreply.github.com>
2021-09-22 07:21:50 -0700

    PetugasSensus

---
+++ PetugasSensus.java
@@ -0,0 +1,18 @@
+public class PetugasSensus {
+    public String nama;
+    public String alamat;
+    public String jenisKelamin;
+    public int nomorTelpon;
+    
+    public void mencatatDataPenduduk(){
+         System.out.println("--DATA PENDUDUK--");
+         System.out.println("NIK : 3511159601010001");
+         System.out.println("Nama : Azimatul Hanafiyah");
+         System.out.println("TTL : Bondowoso, 6 Januari 2001");
+         System.out.println("Alamat : jl. Kenangan gebetan");
+         System.out.println("Agama : Islam");
+         System.out.println("Jenis Kelamin : Perempuan");
+         System.out.println("Status : Belum Kawin");
+         }
+    
+}
